7b850942502974cec3852d0b5a215b6e53a92c3a
ThePywon <ThePywon@hotmail.com>
2023-03-15 21:57:17 -0400

    Small tweaks + Added Vector3

--- src/main.rs
+++ src/main.rs
@@ -1,21 +1,19 @@
 mod math;
-use math::vectors::Vector2;
-
-fn main() {
-	let a = Vector2 { x: 2, y: 5 };
-	let b = Vector2 { x: 3, y: 2 };
-	let c = &a * &b;
-
-	dbg!(&a);
-	dbg!(&b);
-	dbg!(&c);
-	//println!("\"c\" has a magnitude of {} and an angle of {}", c.get_magnitude(), c.get_deg_dir());
-}
-
-/*
+use math::vectors::Vector3;
 use glfw::Context;
 
 fn main() {
+	let a = Vector3::new(2, 5, 3);
+	let b = Vector3::new(3, 2, 4);
+	let c = &a * 2 - &b;
+
+	println!("{}", a);
+	println!("{}", b);
+	println!("{}", c);
+	println!("\"a\" has a magnitude of {} and an angle of {}", a.get_magnitude(), a.get_deg_dir());
+	println!("\"b\" has a magnitude of {} and an angle of {}", b.get_magnitude(), b.get_deg_dir());
+	println!("\"c\" has a magnitude of {} and an angle of {}", c.get_magnitude(), c.get_deg_dir());
+
 	let mut glfw = glfw::init(glfw::FAIL_ON_ERRORS).unwrap();
 
 	let (mut window, _) = glfw.create_window(300, 300,
@@ -30,4 +28,3 @@ fn main() {
 		glfw.poll_events();
 	}
 }
-*/
--- src/math/vectors.rs
+++ src/math/vectors.rs
@@ -1,16 +1,21 @@
 use num_traits::cast::AsPrimitive;
 use num_traits::Num;
 use std::ops::{Add, Sub, Mul};
+use std::fmt::{Display, Formatter, Result};
 
 // Vector2 definition
-#[derive(Debug)]
-pub struct Vector2<T: Num + AsPrimitive<f64>> {
+#[derive(Clone, Copy)]
+pub struct Vector2<T: Num + Display + AsPrimitive<f64>> {
 	pub x: T,
 	pub y: T
 }
 
 // Vector2 methods definition
-impl<T: Num + AsPrimitive<f64>> Vector2<T> {
+impl<T: Num + Display + AsPrimitive<f64>> Vector2<T> {
+	pub fn new(x: T, y: T) -> Self {
+		Vector2 { x, y }
+	}
+
 	pub fn get_magnitude(&self) -> f64 {
 		let x = self.x.as_();
 		let y = self.y.as_();
@@ -30,29 +35,35 @@ impl<T: Num + AsPrimitive<f64>> Vector2<T> {
 	}
 }
 
+impl<T: Num + Display + AsPrimitive<f64>> Display for Vector2<T> {
+	fn fmt(&self, f: &mut Formatter) -> Result {
+		write!(f, "({}, {})", self.x, self.y)
+	}
+}
+
 // Vector2 add operator definition
-impl<T: Num + AsPrimitive<f64>> Add<Vector2<T>> for Vector2<T> {
+impl<T: Num + Display + AsPrimitive<f64>> Add<Vector2<T>> for Vector2<T> {
 	type Output = Vector2<T>;
 
 	fn add(self, rhs: Vector2<T>) -> Vector2<T> {
 		Vector2 { x: self.x + rhs.x, y: self.y + rhs.y }
 	}
 }
-impl<T: Num + AsPrimitive<f64>> Add<&Vector2<T>> for Vector2<T> {
+impl<T: Num + Display + AsPrimitive<f64>> Add<&Vector2<T>> for Vector2<T> {
 	type Output = Vector2<T>;
 
 	fn add(self, rhs: &Vector2<T>) -> Vector2<T> {
 		Vector2 { x: self.x + rhs.x, y: self.y + rhs.y }
 	}
 }
-impl<T: Num + AsPrimitive<f64>> Add<Vector2<T>> for &Vector2<T> {
+impl<T: Num + Display + AsPrimitive<f64>> Add<Vector2<T>> for &Vector2<T> {
 	type Output = Vector2<T>;
 
 	fn add(self, rhs: Vector2<T>) -> Vector2<T> {
 		Vector2 { x: self.x + rhs.x, y: self.y + rhs.y }
 	}
 }
-impl<T: Num + AsPrimitive<f64>> Add<&Vector2<T>> for &Vector2<T> {
+impl<T: Num + Display + AsPrimitive<f64>> Add<&Vector2<T>> for &Vector2<T> {
 	type Output = Vector2<T>;
 
 	fn add(self, rhs: &Vector2<T>) -> Vector2<T> {
@@ -61,28 +72,28 @@ impl<T: Num + AsPrimitive<f64>> Add<&Vector2<T>> for &Vector2<T> {
 }
 
 // Vector2 sub operator definition
-impl<T: Num + AsPrimitive<f64>> Sub<Vector2<T>> for Vector2<T> {
+impl<T: Num + Display + AsPrimitive<f64>> Sub<Vector2<T>> for Vector2<T> {
 	type Output = Vector2<T>;
 
 	fn sub(self, rhs: Vector2<T>) -> Vector2<T> {
 		Vector2 { x: self.x - rhs.x, y: self.y - rhs.y }
 	}
 }
-impl<T: Num + AsPrimitive<f64>> Sub<&Vector2<T>> for Vector2<T> {
+impl<T: Num + Display + AsPrimitive<f64>> Sub<&Vector2<T>> for Vector2<T> {
 	type Output = Vector2<T>;
 
 	fn sub(self, rhs: &Vector2<T>) -> Vector2<T> {
 		Vector2 { x: self.x - rhs.x, y: self.y - rhs.y }
 	}
 }
-impl<T: Num + AsPrimitive<f64>> Sub<Vector2<T>> for &Vector2<T> {
+impl<T: Num + Display + AsPrimitive<f64>> Sub<Vector2<T>> for &Vector2<T> {
 	type Output = Vector2<T>;
 
 	fn sub(self, rhs: Vector2<T>) -> Vector2<T> {
 		Vector2 { x: self.x - rhs.x, y: self.y - rhs.y }
 	}
 }
-impl<T: Num + AsPrimitive<f64>> Sub<&Vector2<T>> for &Vector2<T> {
+impl<T: Num + Display + AsPrimitive<f64>> Sub<&Vector2<T>> for &Vector2<T> {
 	type Output = Vector2<T>;
 
 	fn sub(self, rhs: &Vector2<T>) -> Vector2<T> {
@@ -91,28 +102,28 @@ impl<T: Num + AsPrimitive<f64>> Sub<&Vector2<T>> for &Vector2<T> {
 }
 
 // Vector2 with Vector2 mul operator definition
-impl<T: Num + AsPrimitive<f64>> Mul<Vector2<T>> for Vector2<T> {
+impl<T: Num + Display + AsPrimitive<f64>> Mul<Vector2<T>> for Vector2<T> {
 	type Output = T;
 
 	fn mul(self, rhs: Vector2<T>) -> T {
 		self.x * rhs.x + self.y * rhs.y
 	}
 }
-impl<T: Num + AsPrimitive<f64>> Mul<&Vector2<T>> for Vector2<T> {
+impl<T: Num + Display + AsPrimitive<f64>> Mul<&Vector2<T>> for Vector2<T> {
 	type Output = T;
 
 	fn mul(self, rhs: &Vector2<T>) -> T {
 		self.x * rhs.x + self.y * rhs.y
 	}
 }
-impl<T: Num + AsPrimitive<f64>> Mul<Vector2<T>> for &Vector2<T> {
+impl<T: Num + Display + AsPrimitive<f64>> Mul<Vector2<T>> for &Vector2<T> {
 	type Output = T;
 
 	fn mul(self, rhs: Vector2<T>) -> T {
 		self.x * rhs.x + self.y * rhs.y
 	}
 }
-impl<T: Num + AsPrimitive<f64>> Mul<&Vector2<T>> for &Vector2<T> {
+impl<T: Num + Display + AsPrimitive<f64>> Mul<&Vector2<T>> for &Vector2<T> {
 	type Output = T;
 
 	fn mul(self, rhs: &Vector2<T>) -> T {
@@ -121,17 +132,162 @@ impl<T: Num + AsPrimitive<f64>> Mul<&Vector2<T>> for &Vector2<T> {
 }
 
 // Vector2 with scalar mul operator definition
-impl<T: Num + AsPrimitive<f64>> Mul<T> for Vector2<T> {
+impl<T: Num + Display + AsPrimitive<f64>> Mul<T> for Vector2<T> {
 	type Output = Vector2<T>;
 
 	fn mul(self, rhs: T) -> Vector2<T> {
 		Vector2 { x: self.x * rhs, y: self.y * rhs }
 	}
 }
-impl<T: Num + AsPrimitive<f64>> Mul<T> for &Vector2<T> {
+impl<T: Num + Display + AsPrimitive<f64>> Mul<T> for &Vector2<T> {
 	type Output = Vector2<T>;
 
 	fn mul(self, rhs: T) -> Vector2<T> {
 		Vector2 { x: self.x * rhs, y: self.y * rhs }
+	}
+}
+
+#[derive(Clone, Copy)]
+pub struct Vector3<T: Num + Display + AsPrimitive<f64>> {
+	pub x: T,
+	pub y: T,
+	pub z: T
+}
+
+impl<T: Num + Display + AsPrimitive<f64>> Vector3<T> {
+	pub fn new(x: T, y: T, z: T) -> Self {
+		Vector3 { x, y, z }
+	}
+
+	pub fn get_magnitude(&self) -> f64 {
+		let x = self.x.as_();
+		let y = self.y.as_();
+		let z = self.z.as_();
+
+		(x*x + y*y + z*z).sqrt()
+	}
+
+	pub fn get_rad_dir(&self) -> f64 {
+		let x = self.x.as_();
+		let y = self.y.as_();
+		let z = self.z.as_();
+
+		(z.hypot(y)/x).atan()
+	}
+
+	pub fn get_deg_dir(&self) -> f64 {
+		self.get_rad_dir() * 57.2958f64
+	}
+}
+
+impl<T: Num + Display + AsPrimitive<f64>> Display for Vector3<T> {
+	fn fmt(&self, f: &mut Formatter) -> Result {
+		write!(f, "({}, {}, {})", self.x, self.y, self.z)
+	}
+}
+
+// Vector3 add operator definition
+impl<T: Num + Display + AsPrimitive<f64>> Add<Vector3<T>> for Vector3<T> {
+	type Output = Vector3<T>;
+
+	fn add(self, rhs: Vector3<T>) -> Vector3<T> {
+		Vector3 { x: self.x + rhs.x, y: self.y + rhs.y, z: self.z + rhs.z }
+	}
+}
+impl<T: Num + Display + AsPrimitive<f64>> Add<&Vector3<T>> for Vector3<T> {
+	type Output = Vector3<T>;
+
+	fn add(self, rhs: &Vector3<T>) -> Vector3<T> {
+		Vector3 { x: self.x + rhs.x, y: self.y + rhs.y, z: self.z + rhs.z }
+	}
+}
+impl<T: Num + Display + AsPrimitive<f64>> Add<Vector3<T>> for &Vector3<T> {
+	type Output = Vector3<T>;
+
+	fn add(self, rhs: Vector3<T>) -> Vector3<T> {
+		Vector3 { x: self.x + rhs.x, y: self.y + rhs.y, z: self.z + rhs.z }
+	}
+}
+impl<T: Num + Display + AsPrimitive<f64>> Add<&Vector3<T>> for &Vector3<T> {
+	type Output = Vector3<T>;
+
+	fn add(self, rhs: &Vector3<T>) -> Vector3<T> {
+		Vector3 { x: self.x + rhs.x, y: self.y + rhs.y, z: self.z + rhs.z }
+	}
+}
+
+// Vector3 sub operator definition
+impl<T: Num + Display + AsPrimitive<f64>> Sub<Vector3<T>> for Vector3<T> {
+	type Output = Vector3<T>;
+
+	fn sub(self, rhs: Vector3<T>) -> Vector3<T> {
+		Vector3 { x: self.x - rhs.x, y: self.y - rhs.y, z: self.z - rhs.z }
+	}
+}
+impl<T: Num + Display + AsPrimitive<f64>> Sub<&Vector3<T>> for Vector3<T> {
+	type Output = Vector3<T>;
+
+	fn sub(self, rhs: &Vector3<T>) -> Vector3<T> {
+		Vector3 { x: self.x - rhs.x, y: self.y - rhs.y, z: self.z - rhs.z }
+	}
+}
+impl<T: Num + Display + AsPrimitive<f64>> Sub<Vector3<T>> for &Vector3<T> {
+	type Output = Vector3<T>;
+
+	fn sub(self, rhs: Vector3<T>) -> Vector3<T> {
+		Vector3 { x: self.x - rhs.x, y: self.y - rhs.y, z: self.z - rhs.z }
+	}
+}
+impl<T: Num + Display + AsPrimitive<f64>> Sub<&Vector3<T>> for &Vector3<T> {
+	type Output = Vector3<T>;
+
+	fn sub(self, rhs: &Vector3<T>) -> Vector3<T> {
+		Vector3 { x: self.x - rhs.x, y: self.y - rhs.y, z: self.z - rhs.z }
+	}
+}
+
+// Vector3 with Vector3 mul operator definition
+impl<T: Num + Display + AsPrimitive<f64>> Mul<Vector3<T>> for Vector3<T> {
+	type Output = T;
+
+	fn mul(self, rhs: Vector3<T>) -> T {
+		self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
+	}
+}
+impl<T: Num + Display + AsPrimitive<f64>> Mul<&Vector3<T>> for Vector3<T> {
+	type Output = T;
+
+	fn mul(self, rhs: &Vector3<T>) -> T {
+		self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
+	}
+}
+impl<T: Num + Display + AsPrimitive<f64>> Mul<Vector3<T>> for &Vector3<T> {
+	type Output = T;
+
+	fn mul(self, rhs: Vector3<T>) -> T {
+		self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
+	}
+}
+impl<T: Num + Display + AsPrimitive<f64>> Mul<&Vector3<T>> for &Vector3<T> {
+	type Output = T;
+
+	fn mul(self, rhs: &Vector3<T>) -> T {
+		self.x * rhs.x + self.y * rhs.y + self.z * rhs.z
+	}
+}
+
+// Vector3 with scalar mul operator definition
+impl<T: Num + Display + AsPrimitive<f64>> Mul<T> for Vector3<T> {
+	type Output = Vector3<T>;
+
+	fn mul(self, rhs: T) -> Vector3<T> {
+		Vector3 { x: self.x * rhs, y: self.y * rhs, z: self.z * rhs }
+	}
+}
+impl<T: Num + Display + AsPrimitive<f64>> Mul<T> for &Vector3<T> {
+	type Output = Vector3<T>;
+
+	fn mul(self, rhs: T) -> Vector3<T> {
+		Vector3 { x: self.x * rhs, y: self.y * rhs, z: self.z * rhs }
 	}
 }
