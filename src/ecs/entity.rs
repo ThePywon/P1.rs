ca6f564af247c7e42cc5c7746cb0a2a43ad97f57
ThePywon <ThePywon@hotmail.com>
2023-09-16 19:58:24 -0400

    temporary entity id generator

--- src/ecs/entity.rs
+++ src/ecs/entity.rs
@@ -3,17 +3,25 @@ use std::collections::HashMap;
 
 pub struct Scene {
   component_pools: HashMap<usize, HashMap<usize, Box<dyn Component>>>,
-  pub entities: HashMap<usize, usize>
+  pub entities: HashMap<usize, usize>,
+  next_entity_id: usize
 }
 
 impl Scene {
   pub fn new() -> Self {
-    Scene { component_pools: HashMap::new(), entities: HashMap::new() }
+    Scene { component_pools: HashMap::new(), entities: HashMap::new(), next_entity_id: 0 }
+  }
+
+  fn create_entity_id(&mut self) -> usize {
+    let result = self.next_entity_id;
+    self.next_entity_id += 1;
+    result
   }
 
   pub fn create_entity(&mut self) -> usize {
-    self.entities.insert(0, 0);
-    0
+    let id = self.create_entity_id();
+    self.entities.insert(id, 0);
+    id
   }
 
   pub fn add_component<C: Component + 'static>(&mut self, entity: usize, component: Box<dyn Component>) {
